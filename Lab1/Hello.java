4fe813727dc07e19223cba25cb54376c262088a3
Dudsztor <dudsztor.dudsztor@gmail.com>
2025-10-06 17:43:47 +0200

    choinka

--- Lab1/Hello.java
+++ Lab1/Hello.java
@@ -1,5 +1,16 @@
 public class Hello {
-public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int ilosc_rzedow = Integer.parseInt(args[0]);
+
+        int licznik = 1;
+
+        String gwiazdka = "*";
+
+        while(licznik<ilosc_rzedow+1){
+            System.out.println(gwiazdka.repeat(licznik));
+            licznik++;
+        }
+    }
 }
